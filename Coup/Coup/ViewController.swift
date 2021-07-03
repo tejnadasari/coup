fe61d834019552fd0d047911d6af988b1b20ae8f
Xinyi Zhu <juliezhu212@outlook.com>
2021-07-03 13:04:48 -0500

    Create Xcode project and add app icon

---
+++ Coup/Coup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coup
+//
+//  Created by Xinyi Zhu on 7/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
