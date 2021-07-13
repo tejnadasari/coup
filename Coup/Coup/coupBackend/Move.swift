8acd75a6e307bfce608813cd25b1d43089370b0d
Xinyi Zhu <juliezhu212@outlook.com>
2021-07-13 16:31:04 -0500

    add content for Move class

--- Coup/Coup/coupBackend/Move.swift
+++ Coup/Coup/coupBackend/Move.swift
@@ -1,3 +1,11 @@
 class Move{
-  
+    var moveName: String
+    var callerPlayer: String
+    var targetPlayer: String
+    
+    init(move: String, caller: String, target: String) {
+        moveName = move
+        callerPlayer = caller
+        targetPlayer = target
+    }
 }
