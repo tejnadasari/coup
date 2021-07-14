149e9d8d5bfc60398dcba40ef3f84d19e4367ae5
Yash Patil <yashpatil@Yashs-Air.attlocal.net>
2021-07-14 18:42:12 -0500

    edig

--- Coup/Coup/coupBackend/Game.swift
+++ Coup/Coup/coupBackend/Game.swift
@@ -1,3 +1,41 @@
 class Game{
-  
+
+  var all: [Player]
+  func turn(){
+
+    var turnInd = 0
+    var gameOver = false
+
+    while !gameOver{
+      curMove = getPlayerMove()
+
+      if let challenge = anyChallenges(){
+        if all[turnInd].haveCard(){
+          challenge.callerPlayer.revealCard()
+        }
+        else{
+          challenge.targetPlayer.revealCard()
+        }
+      }
+      else{
+        actOnMove(curMove)
+      }
+      if gameIsOver(){
+        break
+      }
+    }
+  }
+
+  func actOnMove(move: Move){
+    switch move.moveName{
+    case "income":
+      move.
+    }
+  }
+  func incrementInd(ind: inout Int){
+    ind += 1
+    if ind >= all.count{
+      ind = 0
+    }
+  }
 }
