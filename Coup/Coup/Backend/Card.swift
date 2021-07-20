f5a1aaac3d6deb1e7fcc83f4df3ddd2516ec4977
Sungwook Kim - William <brian94kr@gmail.com>
2021-07-20 11:00:17 -0500

    Card.swift & Deck.swift modified

--- Coup/Coup/Backend/Card.swift
+++ Coup/Coup/Backend/Card.swift
@@ -1,3 +1,93 @@
+/*
+ 
+ - At the start of their turn, a player performs one of several actions. Some actions can always be played, while others require a specific role:
+ 
+ 1. getIncome - Draw $1 of income
+ 2. getForeignAid - Draw $2 of foreign aid (duke can block)
+ 3. stageCoup - Pay $7 to stage a coup -> forcing a player of your choice to
+               reveal one influence (cannot be blocked)
+ 4 .tax(duke) - The duke can claim tax, gaining $3
+ 5. assassinate(assassin) - The assassin can pay $3 to make a player of their choice
+                 reveal an influence (if the targeted player has the contessa,
+                 they can block the assassination)
+ 6. exchange2Roles(ambassador) - The ambassador can draw two new roles and choose to
+              exchange any of them with their remaining hidden influences
+ 7. steal(captain) - The captain can steal $2 from another player (if the
+           targeted player has the ambassador or the captain, they can block)
+ 8. block
+ 9. challenge
+
+ Assassin
+ Ambassador
+ Captain
+ Contessa
+ Duke
+
+ -> After playing an action, the other players have an opportunity to block the action or to challenge it. After this the turn passes to the next player.
+ 
+ 
+ */
+
 class Card{
-  
+    var tax:Bool?
+    var assassinate:Bool?
+    var exchange2Roles:Bool?
+    var steal:Bool?
+    
+
+}
+
+class Assassin: Card {
+    
+    override init() {
+        tax = false
+        assassinate = true
+        exchange2Roles = false
+        steal = false
+    }
+    
+}
+
+class Ambassador: Card {
+    
+    override init() {
+        tax = false
+        assassinate = true
+        exchange2Roles = false
+        steal = false
+    }
+    
+}
+
+class Captain: Card {
+    
+    override init() {
+        tax = false
+        assassinate = true
+        exchange2Roles = false
+        steal = false
+    }
+    
+}
+
+class Contessa: Card {
+    
+    override init() {
+        tax = false
+        assassinate = true
+        exchange2Roles = false
+        steal = false
+    }
+    
+}
+
+class Duke: Card {
+    
+    override init() {
+        tax = false
+        assassinate = true
+        exchange2Roles = false
+        steal = false
+    }
+    
 }
--- Coup/Coup/Backend/Deck.swift
+++ Coup/Coup/Backend/Deck.swift
@@ -1,2 +1,60 @@
+/*
+ 
+ Assassin
+ Ambassador
+ Captain
+ Contessa
+ Duke
+ 
+ */
+
 class Deck{
+    var cardDeck = [Any](repeating: 0, count: 15)
+    var count = 3
+    
+    init() {
+        makeDeck()
+    }
+    
+    func makeDeck() {
+        let assassin = Assassin()
+        let ambassador = Ambassador()
+        let captain = Captain()
+        let contessa = Contessa()
+        let duke = Duke()
+        
+        shuffleDeck3(card: assassin)
+        shuffleDeck3(card: ambassador)
+        shuffleDeck3(card: captain)
+        shuffleDeck3(card: contessa)
+        shuffleDeck3(card: duke)
+        
+    }
+    
+    func shuffleDeck3(card: Card) {
+        while(count != 0) {
+            while(true) {
+                let num = Int.random(in: 0...14)
+                
+                if cardDeck[num] as! Int == 0 {
+                    cardDeck[num] = card
+                    break
+                }
+            }
+            count -= 1
+        }
+        
+    }
+    
+    func giveACard() -> Card {
+        let card = cardDeck[0]
+        cardDeck.remove(at: 0)
+        
+        return card as! Card
+    }
+    
+//    func shuffleAndGive() {
+//
+//    }
+
 }
