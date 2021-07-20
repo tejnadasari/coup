7bed8026c3ce910be9ae05773dd851412623ce12
Xinyi Zhu <juliezhu212@outlook.com>
2021-07-20 00:32:00 -0500

    add Firebase Authentication

--- Coup/Coup/AppDelegate.swift
+++ Coup/Coup/AppDelegate.swift
@@ -6,16 +6,19 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+            [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
-    }
+      }
 
     // MARK: UISceneSession Lifecycle
 
