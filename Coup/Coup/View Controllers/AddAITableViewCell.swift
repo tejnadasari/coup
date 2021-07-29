e8eb18ebd890fe3d0fa36a64e5ff9bb8d8d40497
Sungwook Kim - William <brian94kr@gmail.com>
2021-07-29 04:42:10 -0500

    add AddAITableViewCell.swift

---
+++ Coup/Coup/View Controllers/AddAITableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AddAITableViewCell.swift
+//  Coup
+//
+//  Created by Sungwook Kim on 2021/07/29.
+//
+
+import UIKit
+
+class AddAITableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var aiLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
