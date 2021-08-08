2f0c36e4047692c9650005646d636ea5d684704b
Xinyi Zhu <juliezhu212@outlook.com>
2021-08-08 08:46:18 -0500

    Add GameEndsVC

---
+++ Coup/Coup/View Controllers/BlankViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BlankViewController.swift
+//  Coup
+//
+//  Created by Xinyi Zhu on 8/8/21.
+//
+
+import UIKit
+
+class BlankViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Coup/Coup/View Controllers/GameEndsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CheatSheetViewController.swift
+//  Coup
+//
+//  Created by Xinyi Zhu on 7/3/21.
+//
+
+import UIKit
+import AVFoundation
+
+class GameEndsViewController: UIViewController {
+
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    var delegate: UIViewController!
+    var status = "You Lost"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statusLabel.text = ""
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        typeStatus()
+        spinStatus()
+    }
+    
+    func typeStatus() {
+        for char in status {
+            AudioServicesPlaySystemSound(1306)
+            statusLabel.text! += "\(char)"
+            RunLoop.current.run(until: Date() + 0.12)
+        }
+    }
+    
+    func spinStatus() {
+        let durationValue = 3.0
+        
+        UIView.animate(
+            withDuration: durationValue,
+            delay: 1.0,
+            options: [.repeat, .autoreverse],
+            animations: {
+                // 180 degree rotation
+                self.statusLabel.transform =
+                    self.statusLabel.transform.rotated(by: CGFloat(Double.pi))
+                }
+        )
+    }
+    
+    @IBAction func playAgainPressed(_ sender: Any) {
+        statusLabel.layer.removeAllAnimations()
+        self.performSegue(withIdentifier: "unwindToMainVC", sender: self)
+    }
+}
