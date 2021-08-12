c36c89bac612ce0193cc5f73f138350fdd91a661
Xinyi Zhu <juliezhu212@outlook.com>
2021-08-12 06:53:57 -0500

    Update HowToPlayViewController

--- Coup/Coup/View Controllers/HowToPlayViewController.swift
+++ Coup/Coup/View Controllers/HowToPlayViewController.swift
@@ -16,7 +16,6 @@ class HowToPlayViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         howToPlayLabel.text! = "How to Play?"
                
         descriptionLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 1500))
@@ -26,13 +25,13 @@ class HowToPlayViewController: UIViewController, UIScrollViewDelegate {
             Game Objectives:
             
             1. At the start of the game, each player is given 2 roles (cards) which are confidential from the other players & $2
-            2. The goal is for each player to reveal the others' cards through several actions (some involve the money).
+            2. The goal is for each player to reveal the others' cards through several actions (some involve money).
             3. When both the cards of a player are revealed, they are kicked out of the game.
             
             
-            Winner: The last player who has one or more unrevealed cards wins.
+            Winner: The last player who has one or more unrevealed card(s) wins.
 
-            Game Roles(cards) & Actions:
+            Game Roles (cards) & Actions:
             Note: At the end of each player's turn: everyone else can challenge or allow the move.
             
             1. Ambassador
@@ -48,18 +47,17 @@ class HowToPlayViewController: UIViewController, UIScrollViewDelegate {
             
             Everyone
             1. Generic Action: Anyone can draw $1 of income
-            2. Foreign Aid: Anyone can draw $2 of foreign aid
-            3. Coup: Once 7 coins are collected, a player can force another to show one of their cards.
+            2. Coup: Once 7 coins are collected, a player can force another to show one of their cards.
             
             Turn Actions:
-            1. Allow: When another player takes can action they can approve it
-            2. Challenge: When another player suspects that someone is bluffing about their cards, they can force the other people to reveal their card.
-                a. If the challenger's notion is correct, then the action will not be carried out, they are forced to discard the card which will then be shuffled into the deck and they will recieve a new card.
-                b. If the challenger's notion is incorrect, then their action will be carried out as normal.
+            1. Allow: When another player takes can action, you can choose to approve it.
+            2. Challenge: When you suspect that someone is bluffing about their cards, you can force the other people to reveal their card.
+                a. If you are correct, then the action will not be carried out. The other player will be forced to discard a card, which will then be shuffled into the deck and they will recieve a new card.
+                b. If you are incorrect, then the other player's action will be carried out as normal.
             
             Additional Information
-            1. There are a total of 15 cards, consisting of 3 each of the roles.
-            2. Treasury: Pool of money that is distributed between throughout the players and game.
+            1. There are a total of 15 cards, consisting 3 of each of the roles.
+            2. Treasury: Pool of money that is distributed among the players and overall game.
             3. Strategy: Bluff so your correct characters are never revealed.
             """
         scrollView.addSubview(descriptionLabel)
@@ -76,15 +74,4 @@ class HowToPlayViewController: UIViewController, UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return descriptionLabel
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
